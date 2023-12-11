5b4a3546b1173060e032b6bd4e90e76281ec008f
amoiiir <mahamzah535@gmail.com>
2023-12-11 10:58:47 +0800

    new phpmailer

--- contact_form.php
+++ contact_form.php
@@ -1,46 +1,35 @@
 <?php
- 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
- 
-//required files
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
- 
-//Create an instance; passing `true` enables exceptions
-if (isset($_POST["send"])) {
- 
-  $mail = new PHPMailer(true);
- 
-    //Server settings
-    $mail->isSMTP();                              //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;             //Enable SMTP authentication
-    $mail->Username   = 'amiriium381@gmail.com';   //SMTP write your email
-    $mail->Password   = 'tkzssksbodxqxnpe';      //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit SSL encryption
-    $mail->Port       = 465;                                    
- 
-    //Recipients
-    $mail->setFrom( $_POST["email"], $_POST["name"]); // Sender Email and name
-    $mail->addAddress('amiriium381@gmail.com');     //Add a recipient email  
-    $mail->addReplyTo($_POST["email"], $_POST["name"]); // reply to sender email
- 
-    //Content
-    $mail->isHTML(true);               //Set email format to HTML
-    $mail->Subject = $_POST["subject"];   // email subject headings
-    $mail->Body    = $_POST["message"]; //email message
-      
-    // Success sent message alert
-    $mail->send();
-    echo
-    " 
-    <script> 
-     alert('Message was sent successfully!');
-     document.location.href = 'index.php';
-    </script>
-    ";
-}
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "amiriium381@gmail.com";
+$mail->Password = "qrrvaoeqfrqucecz";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("amiriium381@gmail.com", "Amir");
+
+// $mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+echo "Email has been sent";
