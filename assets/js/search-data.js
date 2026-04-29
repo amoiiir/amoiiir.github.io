46ed952476fe41dd19a43dd87c34c9482ef78858
amoiiir <101052053+amoiiir@users.noreply.github.com>
2026-04-29 16:22:38 +0000

    Deploying to gh-pages from @ amoiiir/amoiiir.github.io@86a6a9043a91c4052bf24b4a59b5170fe0e67e2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of my cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "My GitHub repositories and contributions.",
